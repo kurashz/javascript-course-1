e506785fe49492d3dbf7d57f7b68091d4925fa88
kurashz <vladislav.kyrashev@gmail.com>
2022-12-07 21:50:11 +0300

    Продолжать с урока 13

---
+++ 1. Арифметические операторы/main.js
@@ -0,0 +1,24 @@
+// Оператор - внутренняя функция js
+
+// Арифметические операторы
+
+const width = 10;
+const height = 5;
+const space = width * height;
+console.log(space);
+
+const newWidth = width - 6;
+console.log(newWidth);
+
+const volume = 2 ** 3; // 2*2*2
+console.log(volume);
+
+// Строки
+
+const city = 'Moscow';
+const street = 'Novoslobodskaya';
+console.log(city + street); // MoscowNovoslobodskaya
+console.log(city + ', ' + street); // Moscow, Novoslobodskaya
+console.log(city + ', ' + street + 5); // Moscow, Novoslobodskaya5
+
+//Операторы присваивания
