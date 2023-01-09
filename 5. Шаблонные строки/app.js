368d885e2dd217a99d2849d634c9e7beb17e90af
kurashz <vladislav.kyrashev@gmail.com>
2023-01-09 22:17:43 +0300

    Продолжать с Урок №18

---
+++ 5. Шаблонные строки/app.js
@@ -0,0 +1,8 @@
+const nameProj = 'Сайт магазина';
+const price = 2000;
+const author = 'Василий';
+const template = author + ' заказал ' + nameProj + ' по цене ' + price;
+console.log(template);
+
+const template2 = `${author} заказал ${nameProj} по цене: ${price}`;
+console.log(template2);
